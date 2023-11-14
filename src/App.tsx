f6c832577f0088a5f5aac57012e2ff8deb0b31e0
Jacopo Mileto <jacopo851@gmail.com>
2023-11-14 19:59:15 +0100

    refix

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
 import "./App.css";
 import Home from "./components/Home";
-import { WalletContextProvider } from "./components/WalletContext";
-import Provider from "./components/Provider";
-import { MetaMaskProvider } from "@metamask/sdk-react";
-
 function App() {
-  return (
-    <MetaMaskProvider
-      debug={false}
-      sdkOptions={{
-        logging: {
-          developerMode: false,
-        },
-        checkInstallationImmediately: false, // This will automatically connect to MetaMask on page load
-        dappMetadata: {
-          name: "Demo React App",
-          url: window.location.host,
-        },
-      }}
-    >
-      <Home />
-    </MetaMaskProvider>
-  );
+  return <Home />;
 }
 
 export default App;
